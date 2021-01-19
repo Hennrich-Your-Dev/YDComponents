d53f71886d833ef781acc7861752feba2eb38c47
Douglas Hennrich <douglashennrich@yourdev.com.br>
2021-01-19 09:28:39 -0300

    Finalizando Snackbar

--- YDB2WComponents/SnackBar/Controller/YDSnackBarView.swift
+++ YDB2WComponents/SnackBar/Controller/YDSnackBarView.swift
@@ -23,6 +23,10 @@ public class YDSnackBarView: UIView {
   // MARK: Properties
   public weak var delegate: YDSnackBarDelegate?
   var parent: UIView
+  var bottomConstraint: NSLayoutConstraint!
+  var removeTimer: Timer?
+  var hideTimer: Timer?
+  var animateDuration: Double = 0.3
 
   // MARK: Life cycle
   public init(parent: UIView) {
@@ -40,6 +44,10 @@ public class YDSnackBarView: UIView {
     ofType type: YDSnackBarType,
     withIcon icon: UIImage? = nil
   ) {
+    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideSnack))
+    tapGesture.numberOfTapsRequired = 1
+    addGestureRecognizer(tapGesture)
+
     switch type {
       case .simple:
         buildSimpleLayout(message: message, icon: icon)
@@ -50,47 +58,161 @@ public class YDSnackBarView: UIView {
   }
 
   private func buildSimpleLayout(message: String, icon: UIImage?) {
-    let width = parent.frame.size.width / 1.5
     let rect = CGRect(
-      x: (parent.frame.size.width - width) / 2,
-      y: parent.frame.size.height,
-      width: width,
-      height: 46
+      x: 0,
+      y: 0,
+      width: parent.frame.size.width,
+      height: 26
     )
     frame = rect
-    layer.zPosition = CGFloat.greatestFiniteMagnitude
+    layer.zPosition = 20
     backgroundColor = UIColor.Zeplin.black
     layer.cornerRadius = 8
-
     parent.addSubview(self)
 
-    bottomAnchor.constraint(equalTo: parent.bottomAnchor, constant: 0).isActive = true
-    leadingAnchor.constraint(equalTo: parent.leadingAnchor, constant: 12).isActive = true
-    trailingAnchor.constraint(equalTo: parent.trailingAnchor, constant: 12).isActive = true
+    let hasIcon = icon != nil
 
     let label = createLabel(message)
     addSubview(label)
 
-    label.bindFrame(top: 16, bottom: 16, leading: 21, trailing: 21, toView: self)
+    label.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      label.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+      label.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16),
+      label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: hasIcon ? 56 : 21),
+      label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -21)
+    ])
 
-    UIView.animate(withDuration: 0.5) {
-      self.bottomAnchor.constraint(equalTo: self.parent.safeAreaLayoutGuide.bottomAnchor, constant: 40).isActive = true
+    if let icon = icon {
+      let imageView = createIcon(icon)
+      addSubview(imageView)
+
+      imageView.translatesAutoresizingMaskIntoConstraints = false
+      NSLayoutConstraint.activate([
+        imageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+        imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 18),
+        imageView.widthAnchor.constraint(equalToConstant: 18),
+        imageView.heightAnchor.constraint(equalToConstant: 16)
+      ])
+    }
+
+    bottomConstraint = bottomAnchor.constraint(equalTo: parent.bottomAnchor, constant: 100)
+
+    translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      bottomConstraint,
+      leadingAnchor.constraint(equalTo: parent.leadingAnchor, constant: 21),
+      trailingAnchor.constraint(equalTo: parent.trailingAnchor, constant: -21)
+    ])
+
+    parent.layoutIfNeeded()
+    showSnack()
+  }
+
+  private func buildWithButtonLayout(message: String, buttonName: String, icon: UIImage?) {
+    let rect = CGRect(
+      x: 0,
+      y: 0,
+      width: parent.frame.size.width,
+      height: 26
+    )
+    frame = rect
+    layer.zPosition = 20
+    backgroundColor = UIColor.Zeplin.black
+    layer.cornerRadius = 8
+    parent.addSubview(self)
+
+    bottomConstraint = bottomAnchor.constraint(equalTo: parent.bottomAnchor, constant: 100)
+
+    translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      bottomConstraint,
+      leadingAnchor.constraint(equalTo: parent.leadingAnchor, constant: 21),
+      trailingAnchor.constraint(equalTo: parent.trailingAnchor, constant: -21)
+    ])
+
+    let hasIcon = icon != nil
+
+    // Label
+    let label = createLabel(message)
+    addSubview(label)
+
+    label.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      label.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+      label.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16),
+      label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: hasIcon ? 56 : 21),
+      label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -132)
+    ])
+
+    // Button
+    let button = createButton(buttonName)
+    addSubview(button)
+
+    button.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      button.centerYAnchor.constraint(equalTo: centerYAnchor),
+      button.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+      button.heightAnchor.constraint(equalToConstant: 40)
+    ])
+
+    // Icon
+    if let icon = icon {
+      let imageView = createIcon(icon)
+      addSubview(imageView)
+
+      imageView.translatesAutoresizingMaskIntoConstraints = false
+      NSLayoutConstraint.activate([
+        imageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+        imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 18),
+        imageView.widthAnchor.constraint(equalToConstant: 22),
+        imageView.heightAnchor.constraint(equalToConstant: 20)
+      ])
+    }
+
+    parent.layoutIfNeeded()
+    showSnack()
+  }
+
+  @objc private func onButtonAction() {
+    delegate?.onSnackAction()
+    hideSnack()
+  }
+
+  @objc private func showSnack() {
+    UIView.animate(withDuration: animateDuration) {
+      self.bottomConstraint?.constant = -(self.parent.safeAreaInsets.bottom + 16)
       self.parent.layoutIfNeeded()
-      //
-    } completion: { done in
-      if done {
-        UIView.animate(withDuration: 0.5, delay: 5) {
-          self.bottomAnchor.constraint(equalTo: self.parent.bottomAnchor, constant: 0).isActive = true
-          self.parent.layoutIfNeeded()
-        } completion: { _ in
-          self.removeFromSuperview()
-        }
+    } completion: { _ in
+      self.hideTimer?.invalidate()
+      self.hideTimer = Timer.scheduledTimer(
+        timeInterval: 5,
+        target: self,
+        selector: #selector(self.hideSnack),
+        userInfo: nil,
+        repeats: false
+      )
+    }
+  }
+
+  @objc private func hideSnack() {
+    hideTimer?.invalidate()
+    removeTimer?.invalidate()
+    removeTimer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(removeItSelf), userInfo: nil, repeats: false)
+
+    DispatchQueue.main.async {
+      self.parent.layoutIfNeeded()
+      self.layoutIfNeeded()
+      UIView.animate(withDuration: self.animateDuration) {
+        self.bottomConstraint?.constant = 100
+        self.parent.layoutIfNeeded()
       }
     }
   }
 
-  private func buildWithButtonLayout(message: String, buttonName: String, icon: UIImage?) {
-    //
+  @objc private func removeItSelf() {
+    removeTimer?.invalidate()
+    removeFromSuperview()
   }
 
   private func createLabel(_ message: String) -> UILabel {
@@ -101,5 +223,21 @@ public class YDSnackBarView: UIView {
     label.textAlignment = .left
     label.numberOfLines = 0
     return label
+  }
+
+  private func createIcon(_ image: UIImage) -> UIImageView {
+    let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 18, height: 16))
+    imageView.image = image
+    imageView.tintColor = .white
+    return imageView
+  }
+
+  private func createButton(_ title: String) -> UIButton {
+    let button = UIButton()
+    button.setTitle(title, for: .normal)
+    button.setTitleColor(UIColor.Zeplin.yellowBranding, for: .normal)
+    button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+    button.addTarget(self, action: #selector(onButtonAction), for: .touchUpInside)
+    return button
   }
 }
