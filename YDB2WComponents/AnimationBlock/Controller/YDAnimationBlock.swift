1408ed32bff8a2e01db9b35a8c8bd5afebd32930
Douglas Hennrich <douglashennrich@yourdev.com.br>
2021-07-07 10:29:28 -0300

    Editando AnimationBlock

--- YDB2WComponents/AnimationBlock/Controller/YDAnimationBlock.swift
+++ YDB2WComponents/AnimationBlock/Controller/YDAnimationBlock.swift
@@ -13,7 +13,20 @@ public class YDAnimationBlock: UIView {
   var icons: [UIImage?] = []
   let sizes: [CGFloat] = [24, 20, 18, 12]
   var columns: [CGFloat] = [-10, 10, 20, 30, 40, 50]
-  public var iconTintColor: UIColor? = UIColor.Zeplin.colorPrimaryLight
+  public static let secondsArray: [Double] = [
+    0.1,
+    0.15,
+    0.2,
+    0.25,
+    0.3,
+    0.5,
+    0.75,
+    1,
+    1.5,
+    2,
+    3
+  ]
+  public var iconTintColor: UIColor? = Zeplin.colorPrimaryLight
 
   // MARK: Init
   public init() {
